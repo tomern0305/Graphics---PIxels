e898c36c1de64ccb902b1ffafd2c6964b9046977
Tomer Natan <tomern0305@gmail.com>
2025-08-06 18:37:07 +0300

    face

--- Graphics/main.cpp
+++ Graphics/main.cpp
@@ -9,8 +9,102 @@ const int HEIGHT = 600;
 
 unsigned char pixels[HEIGHT][WIDTH][3]; //the 3 is rgb
 
+double offset = 0.0;
+
 void init()
 {
+	int i, j;
+	double distance;
+
+
+	//background
+	for (i = 0; i < HEIGHT; i++)
+	{
+		for (j = 0; j < WIDTH; j++)
+		{
+			pixels[i][j][0] = 0;
+			pixels[i][j][1] = 0;
+			pixels[i][j][2] = 255;
+		}
+	}
+
+	//yellow circle
+	for (i = 0;  i < HEIGHT; i++)
+	{
+		for (j = 0; j < WIDTH; j++)
+		{
+			distance = sqrt(pow(i - HEIGHT / 2, 2) + pow(j - HEIGHT / 2, 2));
+			if (distance < 210)
+			{
+				pixels[i][j][0] = 255;
+				pixels[i][j][1] = 255;
+				pixels[i][j][2] = 0;
+			}
+			
+		}
+	}
+
+	//small circle for mouth
+	for (i = 0; i < HEIGHT; i++)
+	{
+		for (j = 0; j < WIDTH; j++)
+		{
+			distance = sqrt(pow(i - HEIGHT / 2, 2) + pow(j - HEIGHT / 2, 2));
+			if (distance < 140 && distance > 130)
+			{
+				pixels[i][j][0] = 0;
+				pixels[i][j][1] = 0;
+				pixels[i][j][2] = 0;
+			}
+
+		}
+	}
+
+	//rectengle for mouth 
+	for (i = 250; i < 440; i++)
+	{
+		for (j = 150; j < 450; j++)
+		{
+				pixels[i][j][0] = 255;
+				pixels[i][j][1] = 255;
+				pixels[i][j][2] = 0;
+			
+		}
+	}
+
+	// left eye
+	for (i = 0; i < HEIGHT; i++)
+	{
+		for (j = 0; j < WIDTH; j++)
+		{
+			distance = sqrt(pow(i - 60 - HEIGHT / 2, 2) + pow(j + 90 - HEIGHT / 2, 2));
+			if (distance < 30)
+			{
+				pixels[i][j][0] = 0;
+				pixels[i][j][1] = 0;
+				pixels[i][j][2] = 0;
+			}
+
+		}
+	}
+
+	//right eye
+	for (i = 0; i < HEIGHT; i++)
+	{
+		for (j = 0; j < WIDTH; j++)
+		{
+			distance = sqrt(pow(i - 60 - HEIGHT / 2, 2) + pow(j - 90 - HEIGHT / 2, 2));
+			if (distance < 30)
+			{
+				pixels[i][j][0] = 0;
+				pixels[i][j][1] = 0;
+				pixels[i][j][2] = 0;
+			}
+
+		}
+	}
+
+
 }
 
 
